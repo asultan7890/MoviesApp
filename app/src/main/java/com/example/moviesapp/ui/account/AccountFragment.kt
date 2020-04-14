1ea59aef2efc429a324226969bd194f3b1c4a7e6
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-14 18:04:21 -0400

    create account layout / add logout

--- app/src/main/java/com/example/moviesapp/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/account/AccountFragment.kt
@@ -1,14 +1,16 @@
 package com.example.moviesapp.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.moviesapp.LoginActivity
 import com.example.moviesapp.R
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment() {
 
@@ -22,10 +24,14 @@ class AccountFragment : Fragment() {
         accountViewModel =
                 ViewModelProviders.of(this).get(AccountViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_account, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        accountViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val logout = root.findViewById<Button>(R.id.logout)
+        logout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
         return root
     }
+
+
 }
