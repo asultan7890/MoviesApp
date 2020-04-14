4a784f544a5307bbd1735ba5226b3a9108fd7ba2
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-14 15:17:25 -0400

    add register activity

--- app/src/main/java/com/example/moviesapp/RegisterActivity.kt
+++ app/src/main/java/com/example/moviesapp/RegisterActivity.kt
@@ -11,55 +11,53 @@ import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
     private lateinit var emailEt: EditText
     private lateinit var passwordEt: EditText
 
-    private lateinit var signupBtn: Button
+    private lateinit var signUpBtn: Button
     private lateinit var loginBtn: Button
-
-    private lateinit var resetPasswordTv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        emailEt = findViewById(R.id.login_email)
-        passwordEt = findViewById(R.id.login_pass)
-
-        signupBtn = findViewById(R.id.register_account)
-        loginBtn = findViewById(R.id.login_button)
+        setContentView(R.layout.activity_register)
 
         auth = FirebaseAuth.getInstance()
 
-        loginBtn.setOnClickListener {
+        emailEt = findViewById(R.id.register_email)
+        passwordEt = findViewById(R.id.register_pass)
+        var confirm = findViewById<TextView>(R.id.register_confirm)
+
+        loginBtn = findViewById(R.id.register)
+        signUpBtn = findViewById(R.id.login_back)
+
+        signUpBtn.setOnClickListener{
             var email: String = emailEt.text.toString()
             var password: String = passwordEt.text.toString()
 
             if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                Toast.makeText(this@LoginActivity, "Please fill all the fields", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
             } else{
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
-                    if(task.isSuccessful) {
-                        Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                         finish()
                     }else {
-                        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
                     }
                 })
             }
         }
 
-        signupBtn.setOnClickListener{
-            val intent = Intent(this, RegisterActivity::class.java)
+        loginBtn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
-
     }
 }
