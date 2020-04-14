0bbe09f95bb3c067fecefb91e044bc7c7c119cf9
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-14 18:29:16 -0400

    update search layout

--- app/src/main/java/com/example/moviesapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/search/SearchFragment.kt
@@ -1,27 +1,25 @@
-package com.example.moviesapp.ui.home
+package com.example.moviesapp.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.moviesapp.R
 
-class HomeFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var searchViewModel: SearchViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        searchViewModel =
+                ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
         /*val textView: TextView = root.findViewById(R.id.button)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
