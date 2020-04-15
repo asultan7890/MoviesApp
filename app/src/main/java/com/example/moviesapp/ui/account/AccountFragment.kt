31ca68747febe5f31d2e0e9f4c3a0c90d615d2b8
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-15 18:05:12 -0400

    add username to account activity

--- app/src/main/java/com/example/moviesapp/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/account/AccountFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.moviesapp.LoginActivity
 import com.example.moviesapp.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class AccountFragment : Fragment() {
 
     private lateinit var accountViewModel: AccountViewModel
+    private lateinit var user: FirebaseUser
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -25,6 +28,9 @@ class AccountFragment : Fragment() {
                 ViewModelProviders.of(this).get(AccountViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_account, container, false)
         val logout = root.findViewById<Button>(R.id.logout)
+        user = FirebaseAuth.getInstance().currentUser!!
+        val userinfo = root.findViewById<TextView>(R.id.userinfo)
+        userinfo.append(user.email)
         logout.setOnClickListener{
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(activity, LoginActivity::class.java)
