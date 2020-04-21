6616aecb4ac88589c9b7e005929a33432be3cc6f
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-21 18:59:45 -0400

    get movie details from api

--- app/src/main/java/com/example/moviesapp/DetailsActivity.kt
+++ app/src/main/java/com/example/moviesapp/DetailsActivity.kt
@@ -1,8 +1,30 @@
 package com.example.moviesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+
+data class Details(
+    val Title: String,
+    val Year: String,
+    val Rated: String,
+    val Released: String,
+    val Runtime: String,
+    val Genre: String,
+    val Director: String,
+    val Actors: String,
+    val Plot: String,
+    val Poster: String,
+    val imdbRating: String,
+    val imdbID: String,
+    val Type: String
+)
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -12,8 +34,45 @@ class DetailsActivity : AppCompatActivity() {
 
         val id = intent.getStringExtra("ID")
         val title = findViewById<TextView>(R.id.detail_title)
-        title.text = id.toString()
+        val poster = findViewById<ImageView>(R.id.poster)
+        val date = findViewById<TextView>(R.id.date)
+        val rating = findViewById<TextView>(R.id.rating)
+        val runtime = findViewById<TextView>(R.id.runtime)
+        val actors = findViewById<TextView>(R.id.actors)
+        val plot = findViewById<TextView>(R.id.plot)
+        val director = findViewById<TextView>(R.id.director)
+        val imdb = findViewById<TextView>(R.id.imdb)
+        val genre = findViewById<TextView>(R.id.genre)
 
 
+
+        val url = "https://www.omdbapi.com/?apikey=cf10626c&i=$id"
+        val gson = Gson()
+
+        //Request and parse the json movie results
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val responses = response.toString()
+                val results = gson.fromJson(responses, Details::class.java)
+
+                title.text = results.Title
+                Picasso.get().load(results.Poster).into(poster)
+                date.text = results.Released
+                rating.text = results.Rated
+                runtime.text = results.Runtime
+                actors.text = results.Actors
+                plot.text = results.Plot
+                director.text = results.Director
+                imdb.text = results.imdbRating
+                genre.text = results.Genre
+            },
+            Response.ErrorListener { error ->
+                title.text = "ERROR: %s".format(error.toString())
+            }
+        )
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 }
