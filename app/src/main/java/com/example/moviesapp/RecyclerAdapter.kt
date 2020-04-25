d123883956b3c6f62d3e1edd35161339f526b960
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-25 13:51:48 -0400

    add titles of favorites

--- app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(val userList: List<String>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class ListAdapter(val userList: MutableList<List<String>>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         val v = LayoutInflater.from(p0?.context).inflate(R.layout.adapter_layout, p0, false)
         return ViewHolder(v);
@@ -16,7 +17,12 @@ class ListAdapter(val userList: List<String>) : RecyclerView.Adapter<ListAdapter
     }
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
 
-        p0.name?.text = userList[p1]
+
+        p0.name?.text = userList[p1][1]
+        p0.name.setOnClickListener {
+            Toast.makeText(p0.name.context, userList[p1][1],Toast.LENGTH_LONG).show()
+        }
+
     }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name = itemView.findViewById<TextView>(R.id.name)
--- app/src/main/java/com/example/moviesapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/favorites/FavoritesFragment.kt
@@ -8,12 +8,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.example.moviesapp.Details
 import com.example.moviesapp.ListAdapter
+import com.example.moviesapp.MySingleton
 import com.example.moviesapp.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 
 class FavoritesFragment : Fragment() {
     private lateinit var user: FirebaseUser
@@ -33,13 +39,34 @@ class FavoritesFragment : Fragment() {
                 if (document != null) {
 
                     val favorites = document.get("favorites") as List<String>
+                    var favlist = mutableListOf<List<String>>()
+                    for (x in favorites.drop(1)) {
+                        val url = "https://www.omdbapi.com/?apikey=cf10626c&i=$x"
+                        val gson = Gson()
 
-                    //Toast.makeText(activity,favorites.toString(),Toast.LENGTH_LONG).show()
+                        //Request and parse the json movie results
+                        val jsonObjectRequest = JsonObjectRequest(
+                            Request.Method.GET, url, null,
+                            Response.Listener { response ->
+                                val responses = response.toString()
+                                val results = gson.fromJson(responses, Details::class.java)
+
+                                val pair = listOf(x,results.Title)
+                                favlist.add(pair)
+                            },
+                            Response.ErrorListener { error ->
+                                Toast.makeText(activity,"Error loading from API: $error", Toast.LENGTH_LONG).show()
+                            })
+
+                        // Access the RequestQueue through your singleton class.
+                        activity?.let { MySingleton.getInstance(it).addToRequestQueue(jsonObjectRequest) } }
+
+                    Thread.sleep(500)
                     val recycler = root.findViewById<RecyclerView>(R.id.recycler)
                     recycler.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
-
-                    val listadapter = ListAdapter(favorites.drop(1))
-                    recycler.adapter = listadapter}}
+                    val listadapter = ListAdapter(favlist)
+                    recycler.adapter = listadapter
+                } }
             .addOnFailureListener { exception ->
                 Toast.makeText(activity,"Error loading from database: $exception", Toast.LENGTH_LONG).show() }
 
