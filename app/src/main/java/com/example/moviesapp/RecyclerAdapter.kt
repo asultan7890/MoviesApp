b64bc2bf90ad77d2af1f9d5073a3510f21fe4021
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-25 14:32:56 -0400

    add intent that connects favorites to details

--- app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.moviesapp
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ListAdapter(val userList: MutableList<List<String>>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val v = LayoutInflater.from(p0?.context).inflate(R.layout.adapter_layout, p0, false)
+        val v = LayoutInflater.from(p0.context).inflate(R.layout.adapter_layout, p0, false)
         return ViewHolder(v);
     }
     override fun getItemCount(): Int {
@@ -17,15 +17,14 @@ class ListAdapter(val userList: MutableList<List<String>>) : RecyclerView.Adapte
     }
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
 
-
         p0.name?.text = userList[p1][1]
         p0.name.setOnClickListener {
-            Toast.makeText(p0.name.context, userList[p1][1],Toast.LENGTH_LONG).show()
+            val intent = Intent(p0.name.context, DetailsActivity::class.java)
+            intent.putExtra("ID", userList[p1][0])
+            p0.name.context.startActivity(intent)
         }
-
     }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name = itemView.findViewById<TextView>(R.id.name)
-
     }
 }
