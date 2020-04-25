f15702563d252d917818e03525b702509f7d2d1d
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-25 14:38:03 -0400

    update recycler adapter

--- app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
@@ -7,21 +7,21 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(val userList: MutableList<List<String>>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val v = LayoutInflater.from(p0.context).inflate(R.layout.adapter_layout, p0, false)
-        return ViewHolder(v);
+class ListAdapter(val favlist: MutableList<List<String>>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(x: ViewGroup, y: Int): ViewHolder {
+        val z = LayoutInflater.from(x.context).inflate(R.layout.adapter_layout, x, false)
+        return ViewHolder(z)
     }
     override fun getItemCount(): Int {
-        return userList.size
+        return favlist.size
     }
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+    override fun onBindViewHolder(x: ViewHolder, y: Int) {
 
-        p0.name?.text = userList[p1][1]
-        p0.name.setOnClickListener {
-            val intent = Intent(p0.name.context, DetailsActivity::class.java)
-            intent.putExtra("ID", userList[p1][0])
-            p0.name.context.startActivity(intent)
+        x.name?.text = favlist[y][1]
+        x.name.setOnClickListener {
+            val intent = Intent(x.name.context, DetailsActivity::class.java)
+            intent.putExtra("ID", favlist[y][0])
+            x.name.context.startActivity(intent)
         }
     }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
