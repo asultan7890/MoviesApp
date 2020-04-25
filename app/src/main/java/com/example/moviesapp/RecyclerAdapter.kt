495683830e654d03482b9d2ae4f71df5eecd34ad
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2020-04-25 12:20:33 -0400

    add recycler to list IDs of favorites

---
+++ app/src/main/java/com/example/moviesapp/RecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.moviesapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(val userList: List<String>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val v = LayoutInflater.from(p0?.context).inflate(R.layout.adapter_layout, p0, false)
+        return ViewHolder(v);
+    }
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+
+        p0.name?.text = userList[p1]
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.name)
+
+    }
+}
--- app/src/main/java/com/example/moviesapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/favorites/FavoritesFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviesapp.ListAdapter
 import com.example.moviesapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FavoritesFragment : Fragment() {
+    private lateinit var user: FirebaseUser
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -18,6 +24,24 @@ class FavoritesFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_favorites, container, false)
+
+        val db = Firebase.firestore
+        user = FirebaseAuth.getInstance().currentUser!!
+        val doc = db.collection("users").document(user.email.toString())
+        doc.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+
+                    val favorites = document.get("favorites") as List<String>
+
+                    //Toast.makeText(activity,favorites.toString(),Toast.LENGTH_LONG).show()
+                    val recycler = root.findViewById<RecyclerView>(R.id.recycler)
+                    recycler.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
+                    val listadapter = ListAdapter(favorites.drop(1))
+                    recycler.adapter = listadapter}}
+            .addOnFailureListener { exception ->
+                Toast.makeText(activity,"Error loading from database: $exception", Toast.LENGTH_LONG).show() }
 
         return root
     }
